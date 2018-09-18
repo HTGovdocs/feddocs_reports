24c9ac67126a3d48069be79c08a76caaf948e833
Josh Steverman <jstever@umich.edu>
2018-09-18 13:22:28 -0400

    gzip zephir snapshot after import

--- snapshot_zephir_source_records.sh
+++ snapshot_zephir_source_records.sh
@@ -1,3 +1,4 @@
 DATE=$(date +\%Y-\%m-\%d)
 mongoexport --quiet -d htgd -c source_records --query '{org_code:"miaahdl"}' -o /l1/govdocs/reports/data/ht_$DATE.json
 mongoimport --quiet -d zephir_$(date +\%Y\%m\%d) -c source_records --file /l1/govdocs/reports/data/ht_$DATE.json
+gzip /l1/govdocs/reports/data/ht_$DATE.json
