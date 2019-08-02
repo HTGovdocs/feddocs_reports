32c1f0c051cb8547ad61706e5c90704fad7f1914
Josh Steverman <jstever@umich.edu>
2019-08-02 12:11:03 -0400

    auto copy reports to blacklight assets

--- monthly_reporting.sh
+++ monthly_reporting.sh
@@ -2,3 +2,8 @@ D=$(date +\%Y-\%m-\%d)
 ./snapshot_zephir_source_records.sh
 bundle exec ruby collection_profile_report.rb $D
 bundle exec ruby stat_overview_report.rb
+
+
+mkdir /htapps/jstever.apps/usdocs_registry/public/assets/$D
+cp reports/num*.csv /htapps/jstever.apps/usdocs_registry/public/assets/stats/
+cp reports/${D}_${D}/* /htapps/jstever.apps/usdocs_registry/public/assets/$D
