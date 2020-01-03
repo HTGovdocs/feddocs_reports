24b4d4a23dfe29a6bc52acada290d16234eb6e7c
Josh Steverman <jstever@umich.edu>
2020-01-03 11:59:00 -0500

    2020-01 reports. Renaming of output directory

--- monthly_reporting.sh
+++ monthly_reporting.sh
@@ -4,6 +4,6 @@ bundle exec ruby collection_profile_report.rb $D
 bundle exec ruby stat_overview_report.rb
 
 
-mkdir /htapps/jstever.apps/usdocs_registry/public/assets/$D
-cp reports/num*.csv /htapps/jstever.apps/usdocs_registry/public/assets/stats/
-cp reports/${D}_${D}/* /htapps/jstever.apps/usdocs_registry/public/assets/$D
+mkdir /htapps-dev/jstever.apps/usdocs_registry/public/assets/$D
+cp reports/num*.csv /htapps-dev/jstever.apps/usdocs_registry/public/assets/stats/
+cp reports/${D}_${D}/* /htapps-dev/jstever.apps/usdocs_registry/public/assets/$D
